eb5c9705dd282199a41223d85387baceebeda88d
David Munyiri <38491805+davy254@users.noreply.github.com>
2025-08-26 17:18:35 +0300

    rendering list of tasks

--- taskforge/tasks/models.py
+++ taskforge/tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Projects"
+        ordering =["-created_at"]
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return f"<Project(name={self.name}, created_at={self.created_at})>"
--- taskforge/tasks/views.py
+++ taskforge/tasks/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def home(request):
+    """Render the home page."""
+    
     return render(request, "tasks/home.html")
+
+
+def project_list(request):
+    """Render the projects on homepage."""
+    projects = Project.objects.all()
+    
+    context = {
+        'projects': projects
+    }
+    return render(request, "tasks/project_list.html", context)
+
+
